6b3a3fac3d45640e85c7f80be6e37cab17554db4
Dmitry Zhutkov <dmitry.zhutkov@gmail.com>
2021-03-23 15:02:14 +0500

    First commit

---
+++ assets/js/translate.js
@@ -0,0 +1,16 @@
+var translateStrings = {
+    'en': {
+        'ToLearnMore': 'To learn more',
+        'ARevolutionarySolution': 'A revolutionary solution',
+        'Description1': 'This is a program for editing package files of games from Maxis. It also has several modules for efficient mod development.',
+        'Module': 'The project is under development, try its module!',
+        'Description2': 'This program cleans up saves for The Sims 3. It is the fastest, most efficient and safest solution for clearing saves so far.'
+    },
+    'ru': {
+        'ToLearnMore': 'Узнать больше',
+        'ARevolutionarySolution': 'Революционное решение',
+        'Description1': 'Это программа для редактирования package-файлов игр от Maxis. Также он имеет несколько модулей для эффективной разработки модов.',
+        'Module': 'Проект находится на стадии разработки, попробуйте его модуль!',
+        'Description2': 'Это программа очищает сохранения для игры The Sims 3. На данный момент это самое быстрое, эффективное и безопасное решение для очистки сохранении.'
+    }
+}
